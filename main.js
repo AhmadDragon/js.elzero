e3546daa17d2a1b23cc83bea5301b521333b7816
AhmedApple091 <ahmed.apple019@gmail.com>
2022-07-15 13:16:45 +0300

    i don't inderstaind

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 window.alert("Welcom");
+let card = "Elzero";"Elzero_Web_School";"12/10/1991";
+
+let j = `
+<div class="card">
+    <div class="child">
+        <h3>${card}</h3>
+        <p>any words just for webside for lclclclclc</p>
+    </div>
+</div>
+`
+console.log("j".repeat(4));
